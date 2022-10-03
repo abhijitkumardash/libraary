9f5ba15ae3410321657bebe5e8be9c35f3e72370
Andrei Jurcă <107988807+andryjurca@users.noreply.github.com>
2022-10-03 17:21:26 +0200

    displaying a simple table of books

--- src/main/webapp/app/librarian/book/list/book.component.ts
+++ src/main/webapp/app/librarian/book/list/book.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from '../../../entities/book/book.model';
+import { BookService } from '../../../entities/book/book.service';
 
 @Component({
   selector: 'jhi-book-list',
   templateUrl: './book.component.html',
 })
 export class BookComponent implements OnInit {
-
-  constructor() { }
+  displayedColumns: string[] = ['id', 'title', 'author', 'year', 'publisher', 'publicationYear', 'pages', 'isbn'];
+  data: any | null = null;
+  constructor(private bookService: BookService) { }
 
   ngOnInit(): void {
+    this.bookService.query().subscribe((result) => {
+      this.data = result.body
+      console.log(this.data)
+    })
   }
-
+  
+  
 }
