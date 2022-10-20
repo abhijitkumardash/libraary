cb8fe32c7da52e6a07e23aaf1d9152713930ea31
Andrei Jurcă <107988807+andryjurca@users.noreply.github.com>
2022-10-20 23:08:44 +0200

    adding filter and paginator

--- src/main/webapp/app/librarian/book/list/book.component.ts
+++ src/main/webapp/app/librarian/book/list/book.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from '../../../entities/book/book.model';
 import { BookService } from '../../../entities/book/book.service';
+import {AfterViewInit, ViewChild} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'jhi-book-list',
@@ -8,14 +12,32 @@ import { BookService } from '../../../entities/book/book.service';
 })
 export class BookComponent implements OnInit {
   displayedColumns: string[] = ['id', 'title', 'author', 'year', 'publisher', 'publicationYear', 'pages', 'isbn'];
-  data: any | null = null;
+  dataSource: any | null = null;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
   constructor(private bookService: BookService) { }
 
   ngOnInit(): void {
     this.bookService.query().subscribe((result) => {
-      this.data = result.body
-      console.log(this.data)
+      this.dataSource = result.body
+      console.log(this.dataSource)
     })
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
   
   
