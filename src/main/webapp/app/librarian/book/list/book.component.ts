21bf69939af082c9a6bccaa2c8bda3426f806efc
Andrei Jurcă <107988807+andryjurca@users.noreply.github.com>
2022-12-22 18:52:35 +0100

    Fix issue #23

--- src/main/webapp/app/librarian/book/list/book.component.ts
+++ src/main/webapp/app/librarian/book/list/book.component.ts
@@ -4,23 +4,26 @@ import {ViewChild} from '@angular/core';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatSort} from '@angular/material/sort';
 import { BookItemService } from '../../../entities/book-item/bookitem.service';
+import { DomSanitizer } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'jhi-book-list',
   templateUrl: './book.component.html',
 })
 export class BookComponent implements OnInit {
-  displayedColumns: string[] = ['id', 'title', 'author', 'publisher', 'publicationYear', 'pages', 'isbn'];
+  displayedColumns: string[] = ['cover', 'title', 'author', 'isbn', 'year', 'pages', 'format', 'status'];
   dataSource: any | null = null;
-
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
-  constructor(private bookItemService: BookItemService) { }
+  constructor(private bookItemService: BookItemService, protected sanitizer: DomSanitizer) {  }
 
   ngOnInit(): void {
     this.bookItemService.query().subscribe((result) => {
       this.dataSource = result.body
+      console.log(result.body)
+      
     })
   }
 
